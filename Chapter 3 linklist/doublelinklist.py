9ddbe0f1e44266b24609fcd7fb96be26d4eb90dd
vishaljasrotia <vishal.singh8989@gmail.com>
2017-09-21 23:32:03 -0400

    double linklist started

--- Chapter 3 linklist/doublelinklist.py
+++ Chapter 3 linklist/doublelinklist.py
@@ -9,4 +9,47 @@ __status__ = ""
 
 import os
 
-#TODO
+class Node(object):
+
+        def validate(f):
+            def inner(*args, **kwargs):
+                if isinstance(args[1], Node)  or args[1] is None:
+                    f(args, kwargs)
+                else:
+                    raise Exception("next and previous node should be object of class Node or None")
+            return inner
+
+
+        def __new__(cls, data, nextNode, prevNode):
+           if (isinstance(nextNode, Node) or nextNode is None) and (isinstance(prevNode, Node) or prevNode is None):
+               return super(Node, cls).__new__(cls, data, nextNode, prevNode)
+           else:
+               raise Exception("next node should be object of class Node or None")
+
+        def __init__(self, data, nextNode, prevNode):
+            self._data = data
+            self._nextNode = nextNode
+            self._prev = prevNode
+
+        @validate
+        def setNext(self, nextNode):
+            self._nextNode = nextNode
+
+        @validate
+        def setPrev(self, prevNode):
+             self._prevNode = prevNode
+
+        def getData(self):
+            return self._data
+
+        def setData(self, data):
+            self._data = data
+
+        def getNext(self):
+            return self._nextNode
+
+        def getPrev(self):
+            return self._prevNode
+
+node = Node(10, None, None)
+print(node.getData())
