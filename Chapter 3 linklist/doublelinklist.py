368b5d4d74b5e20625f0e2fef50d4200ef07d9b7
vishaljasrotia <vishal.singh8989@gmail.com>
2017-09-27 02:48:30 -0400

    double link list started

--- Chapter 3 linklist/doublelinklist.py
+++ Chapter 3 linklist/doublelinklist.py
@@ -7,14 +7,14 @@ __maintainer__ = "Vishal Jasrotia"
 __email__ = "jasrotia.vishal@stonybrook.edu"
 __status__ = ""
 
-import os
+import os, sys
 
 class Node(object):
 
         def validate(f):
             def inner(*args, **kwargs):
                 if isinstance(args[1], Node)  or args[1] is None:
-                    f(args, kwargs)
+                    f(*args, **kwargs)
                 else:
                     raise Exception("next and previous node should be object of class Node or None")
             return inner
@@ -23,13 +23,15 @@ class Node(object):
         def __new__(cls, data, nextNode, prevNode):
            if (isinstance(nextNode, Node) or nextNode is None) and (isinstance(prevNode, Node) or prevNode is None):
                return super(Node, cls).__new__(cls, data, nextNode, prevNode)
+
            else:
                raise Exception("next node should be object of class Node or None")
 
         def __init__(self, data, nextNode, prevNode):
+            print("__init__ %s ( %d, %s , %s)"%(self,data, nextNode, prevNode))
             self._data = data
             self._nextNode = nextNode
-            self._prev = prevNode
+            self._prevNode = prevNode
 
         @validate
         def setNext(self, nextNode):
@@ -37,7 +39,7 @@ class Node(object):
 
         @validate
         def setPrev(self, prevNode):
-             self._prevNode = prevNode
+            self._prevNode = prevNode
 
         def getData(self):
             return self._data
@@ -51,5 +53,103 @@ class Node(object):
         def getPrev(self):
             return self._prevNode
 
-node = Node(10, None, None)
-print(node.getData())
+
+class DoubleLinklist:
+
+    def __init__(self):
+        self._head = None
+        self._tail = None
+
+    def __len__(self):
+        temp = self._head
+        count = 0
+        while temp is not None:
+            count+=1
+            temp = temp.getNext()
+        return count
+
+    def insertAtBegin(self, data):
+        if self._head is None:
+            self._head = Node(data, None, None)
+            self._tail = self._head
+        else:
+            tempnode = Node(data, self._head, None)
+            self._head.setPrev(tempnode)
+            self._head = tempnode
+
+
+    def insertAtEnd(self, data):
+        if self._tail is None:
+            self._tail = Node(data, None, None)
+            self._head = self._tail
+        else:
+            tempnode = Node(data, None, self._tail)
+            self._tail.setNext(tempnode)
+            self._tail = tempnode
+
+    def deleteFromPosition(self, position):
+        if self._head is None or self._tail is None:
+            print("list is empty")
+        elif position == 0 :
+            temp = self._head
+            self._head = self._head.getNext()
+            if self._head is None:
+                self._tail = None
+            else:
+                self._head.setPrev(None)
+            del temp
+        elif position == len(self):
+            temp = self._tail
+            self._tail = self._tail.getPrev()
+            if self._tail is not None:
+                self._tail.setNext(None)
+            else:
+                self._head = None
+        else:
+
+
+            currentPosition = 0
+            temp = self._head
+            while temp is not None and currentPostion < position - 1 :
+                currentPostion+=1
+                temp = temp.getNext()
+            if temp is None:
+                raise Exception("List len is less than position.")
+            else:
+                temp.getPrev().setNext(temp.getNext())
+
+
+
+
+    def printFromStart(self):
+        temphead = self._head
+        while temphead is not None:
+            sys.stdout.write("%d"%temphead.getData())
+            temphead = temphead.getNext()
+            if temphead is not None:
+                sys.stdout.write(" -> ")
+        print("")
+
+    def printFromEnd(self):
+        temptail = self._tail
+        while temptail is not None:
+            sys.stdout.write("%d"%temptail.getData())
+            temptail = temptail.getPrev()
+            if temptail is not None:
+                sys.stdout.write(" -> ")
+        print("")
+
+if __name__ == "__main__":
+    print("start")
+    dlist  = DoubleLinklist()
+    dlist.insertAtBegin(10)
+    dlist.insertAtBegin(20)
+    dlist.insertAtBegin(30)
+    dlist.insertAtBegin(40)
+    dlist.insertAtEnd(0)
+    dlist.printFromStart()
+    dlist.printFromEnd()
+    dlist.deleteFromPosition(0)
+    dlist.printFromStart()
+    dlist.printFromEnd()
+    print("bye bye")
