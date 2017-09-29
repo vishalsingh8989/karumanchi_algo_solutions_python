8488aa1991e202fe25e43c659b8382bec1e0af23
vishaljasrotia <vishal.singh8989@gmail.com>
2017-09-28 21:25:32 -0400

    stack

---
+++ Chapter 4 stacks/stacks.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+__author__ = "Vishal Jasrotia. Stony Brook University"
+__copyright__ = ""
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Vishal Jasrotia"
+__email__ = "jasrotia.vishal@stonybrook.edu"
+__status__ = ""
+
+import sys
+
+
+class Node:
+    def validate(f):
+        def inner(*args, **kwargs):
+            f(*args, **kwargs)
+
+        return inner
+
+    def __init__(self, data, nextNode):
+        self._data=data
+        self._nexNode = nextNode
+
+
+class Stack:
+    """implementation of stack using python builtin list. 
+        Stack can be implemeted using above node class.
+    """
+    def __init__(self):
+        self._stack = []
+
+    def pop(self):
+        if len(self._stack) == 0:
+            raise Exception("Stack is empty")
+        data = self._stack.pop(0)
+        return data
+
+    def push(self, data):
+        self._stack.insert(0, data)
+    
+    def peek(self):
+        return self._stack[0]
+
+    def printstack(self):
+        for x in self._stack:
+            print("+----------------+")
+            print("|       %d        |"%x)
+        print("------------------")
+
+if __name__ == "__main__":
+    stack = Stack()
+    print("psuh 6,5,4,3 and print stack")
+    stack.push(6)
+    stack.push(5)
+    stack.push(4)
+    stack.push(3)
+    stack.printstack()
+    print("Top is : %d"%stack.pop())
+    print("print after pop")
+    stack.printstack()
+    print("Peek top : %d "%stack.pop())
+    
+
+
